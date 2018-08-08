54b3e5656ec44e81ab2b87dc768e12312da10d6b
ErgoZ Riftbit Vaper <ergozru@gmail.com>
2018-08-08 23:52:31 +0300

    Update error.go

--- error.go
+++ error.go
@@ -3,7 +3,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-package jsonrpc2
+package jrpc2server
 
 // ErrorCode basic error code type
 type ErrorCode int
